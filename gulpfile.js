2ebc2d7e483b2c5a644c18c66506dc309e484e27
Bilal Kazi <bilalahmedkazi@hotmail.com>
2016-02-12 19:46:25 +0000

    Add tasks to handle Sass stylesheets

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 var ts = require('gulp-typescript');
 var browserSync = require('browser-sync').create();
 
@@ -8,6 +10,16 @@ gulp.task('typescript', function() {
         noImplicitAny: true
       }))
       .pipe(gulp.dest('app/'));
+});
+
+gulp.task('styles', function() {
+  gulp.src('src/styles/main.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(autoprefixer({
+        browsers: ['last 2 versions']
+      }))
+      .pipe(gulp.dest('styles/'))
+      .pipe(browserSync.stream());
 });
 
 gulp.task('scripts', function() {
@@ -20,8 +32,9 @@ gulp.task('html', function() {
       .pipe(gulp.dest('./'));
 });
 
-gulp.task('default', ['html', 'scripts'], function() {
+gulp.task('default', ['html', 'styles', 'scripts'], function() {
   // gulp.watch('src/**/*.ts', gulp.task('typescript')).on('change', browserSync.reload);
+  gulp.watch(['src/styles/main.scss'], ['styles']);
   gulp.watch(['src/js/*.js'], ['scripts'])
       .on('change', browserSync.reload);
   gulp.watch(['src/index.html'], ['html'])
