05d5e137dc3915cdd212e54bc04dc3ba14087834
Bilal Kazi <bilalahmedkazi@hotmail.com>
2016-02-12 22:30:40 +0000

    Add initial XHR test to API
    So the API works m8
    Next step would be to get Angular2 up and running to use the JSON data and maybe send the request through Angular and start the actual app

--- src/js/main.js
+++ src/js/main.js
@@ -23,7 +23,21 @@ if ('geolocation' in navigator) {
 /** Gets position using geolocation */
 function getPos() {
   navigator.geolocation.getCurrentPosition(function(position) {
-    console.log(position.coords.latitude);
-    console.log(position.coords.longitude);
+    var lat = position.coords.latitude;
+    var lon = position.coords.longitude;
+    console.log(lat);
+    console.log(lon);
+    var req = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&APPID=aed6185e2ce407f75566d8df9954d81f&units=metric';
+    var oReq = new XMLHttpRequest();
+    oReq.addEventListener('load', function() {
+      var parsed = JSON.parse(this.responseText);
+      console.log('Temperature: ' + parsed.main.temp);
+      console.log('Country: ' + parsed.sys.country);
+      console.log('City: ' + parsed.name);
+    });
+    oReq.open('GET', req);
+    oReq.send();
   });
 }
+
+// api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&APPID=aed6185e2ce407f75566d8df9954d81f
